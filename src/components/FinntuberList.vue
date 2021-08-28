ecaeac0f5dc340566528b00e2879906702801d48
divusmetaphysica <divusmetaphysica@gmail.com>
2021-08-28 21:33:49 +0300

    Midcheck

--- src/components/FinntuberList.vue
+++ src/components/FinntuberList.vue
@@ -2,59 +2,66 @@
   <div>
     <h1>List of Finnish VTubers</h1>
     <div class="row" v-for="ft in finntubers" :key="ft.name">
+      <!-- <div class="column">
+        <link ref="{{ ti.twitch_info.profile_image_url }}" />
+      </div> -->
       <div class="column">
         <b>
-          <a href="{{ ft.channel }}">{{ ft.name }}</a>
+          <a href="{{ ft.channel }}">{{ ft.name }} </a>
         </b>
+        {{ ft.type }}
       </div>
-      <div class="column">{{ ft.type }}</div>
-      <div class="column">{{ ft.short_description }}</div>
-    </div>
-  </div>
-
-  <div>
-    <div v-for="ti in twitch_info" :key="ti.name">
-      <div>{{ ti }}</div>
+      <!-- <div class="column">{{ ft.twitch_info.description }}</div> -->
     </div>
   </div>
 </template>
 
 <script>
 import * as db from "../../finntubers.json";
-import axios from "axios";
+// import axios from "axios";
 
-const oAuthToken = {
-  token: "",
-  client_id: "",
-};
+// const oAuthToken = {
+//   token: "ed5bqclqh3ya32kqg12x8kui38chtk",
+//   client_id: "pjdodziokew6n0yphlgh78011ty6a9",
+// };
 
-const loadTwitchInfo = (arr) => {
-  let data = [];
-  console.log(arr);
-  let user_ids = arr.default
-    .filter((x) => x.channel_name != null)
-    .map((x) => `login=${x.channel_name}`)
-    .join("&");
+// const loadTwitchInfo = async (db) => {
+//   let fetchedData = [];
 
-  const config = {
-    headers: {
-      Authorization: `Bearer ${oAuthToken.token}`,
-      "Client-Id": oAuthToken.client_id,
-    },
-  };
+//   let user_ids = db.default
+//     .filter((x) => x.channel_name != null && x.channel_name != undefined)
+//     .map((x) => `login=${x.channel_name.toLowerCase()}`)
+//     .join("&");
 
-  axios
-    .get(`https://api.twitch.tv/helix/users?${user_ids}`, config)
-    .then((x) => data.push(x))
-    .catch((x) => console.log(x));
-  return data;
-};
+//   const config = {
+//     headers: {
+//       Authorization: `Bearer ${oAuthToken.token}`,
+//       "Client-Id": oAuthToken.client_id,
+//     },
+//   };
 
+//   await axios
+//     .get(`https://api.twitch.tv/helix/users?${user_ids}`, config)
+//     .then((x) => {
+//       console.log(x.data.data);
+//       x.data.data.forEach((element) => fetchedData.push(element));
+//     })
+//     .catch((x) => console.log(x))
+//     .finally(() => console.log("Finished user fetch"));
+
+//   const result = db.default.map((x) => {
+//     if (x.channel_name != null || x.channel_name != undefined) {
+//       x.twitch_info = fetchedData.find((y) => y.login === x.channel_name);
+//     }
+//     return x;
+//   });
+//   console.log(result);
+//   return result;
+// };
 export default {
   name: "Finntubers",
   props: {
     finntubers: db,
-    twitch_info: loadTwitchInfo(db),
   },
 };
 </script>
