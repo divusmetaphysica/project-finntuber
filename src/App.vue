46f594d139285d94c9d3a6889c722229929c1a1d
Mariel <MailForMariel@gmail.com>
2023-11-04 21:21:23 +0200

    Thicker header stroke

--- src/App.vue
+++ src/App.vue
@@ -41,8 +41,8 @@ h1 {
   text-align: center;
   padding: 50px;
   font-family: "Dosis", sans-serif;
-  text-shadow: -3px -3px 0 #fff, 0 -3px 0 #fff, 3px -3px 0 #fff, 3px 0 0 #fff,
-    3px 3px 0 #fff, 0 3px 0 #fff, -3px 3px 0 #fff, -3px 0 0 #fff;
+  text-shadow: -4px -4px 0 #fff, 0 -4px 0 #fff, 4px -4px 0 #fff, 4px 0 0 #fff,
+    4px 4px 0 #fff, 0 4px 0 #fff, -4px 4px 0 #fff, -4px 0 0 #fff;
 }
 .content {
   flex: 1;
