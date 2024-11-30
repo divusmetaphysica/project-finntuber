56f4ada9314ee2d2b8ffb91c6462100235951974
divusmetaphysica <divusmetaphysica@gmail.com>
2024-11-30 17:01:43 +0200

    upgrade cleanup

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import timeago from "vue-timeago3";
-
-createApp(App)
-  .use(router)
-  .use(timeago)
-  .mount("#app");
